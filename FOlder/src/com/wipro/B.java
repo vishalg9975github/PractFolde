e754b37da1ebd5d8a3579dee3bdb710cb9ae8db5
vishalg9975github <130144958+vishalg9975github@users.noreply.github.com>
2023-05-14 10:15:42 +0530

    Update B.java

--- FOlder/src/com/wipro/B.java
+++ FOlder/src/com/wipro/B.java
@@ -5,6 +5,8 @@ public class B {
 	public static void main(String[] args) {
 		
 		System.out.println("Class B");
+		System.out.println("Changes madde in class B");
+		System.out.println("If this file is updated then you can select the files on repository");
 	}
 
 }
